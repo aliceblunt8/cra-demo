a8b24f8d71a2a2fb34e09a4a2b87c3b52d895c50
Alisa Ivashkevych <a.ivashkevych@mate.academy>
2020-10-21 12:29:29 +0300

    Updates

---
+++ precache-manifest.f6bf4eec061729c9775dc7e8f8672c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e83a3a168df787fd31d4e0c8a5e040a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c709329f7b565352796",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "462bf43a20a860f66bfd",
+    "url": "./static/js/2.9c68f122.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.9c68f122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c709329f7b565352796",
+    "url": "./static/js/main.d949b035.chunk.js"
+  },
+  {
+    "revision": "4e0eef9267ed46c1c31c",
+    "url": "./static/js/runtime-main.4d21a353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
